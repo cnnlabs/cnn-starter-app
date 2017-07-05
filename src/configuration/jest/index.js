c20984a5012781aeb9b958f1295c5ebfd5109d1f
Joshua David Clayton <joshdavidclayton@gmail.com>
2017-07-05 09:56:04 -0400

    add test command and configuration

---
+++ src/configuration/jest/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Creates a configuration object to control Jest's behavior.
+ *
+ * {@link https://facebook.github.io/jest/docs/configuration.html#content}
+ * @param {Function} resolve    A resolver function that accepts a relative path
+ * @param {String}   rootDir    The root directory for the tests
+ * @return {Object}             The Jest configuration object
+ */
+const createJestConfig = (resolve, rootDir) => ({
+    rootDir,
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.(js|jsx)$': resolve('configuration/jest/babelTransform')
+    },
+    moduleNameMapper: {
+        '^.+\\.css$': 'identity-obj-proxy'
+    }
+});
+
+module.exports = createJestConfig;
