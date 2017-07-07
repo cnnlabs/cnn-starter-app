99eaa86a755ace7ff22cfffef1ba916b0ab0ee56
Charles Rawls <charles.rawls@turner.com>
2017-07-07 11:45:58 -0400

    Adding option to override NODE_ENV setting for npm run start

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -4,7 +4,7 @@ const resolve = require('path').resolve;
 const spawn = require('child_process').spawn;
 const argv = process.argv;
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const command = 'node';
 const args = [
