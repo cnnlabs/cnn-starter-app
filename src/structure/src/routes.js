e36acd39ba6b4978d729575de3d773bba718b085
Joshua David Clayton <joshdavidclayton@gmail.com>
2017-07-13 16:15:58 -0400

    replace System.import with dynamic import syntax

--- src/structure/src/routes.js
+++ src/structure/src/routes.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import AsyncRoute from 'modules/async-route';
 
-// @TODO: find a better implentation for this
-if (global) { global.System = { import() {} } }
-
 const noMatch = {
     component: (props) => (
-        <AsyncRoute props={props} loadingPromise={System.import('./pages/no-match')} />
+        <AsyncRoute props={props} loadingPromise={import('./pages/no-match')} />
     )
 };
 
@@ -15,21 +12,21 @@ const routes = [
         path: '/',
         exact: true,
         component: (props) => (
-            <AsyncRoute props={props} loadingPromise={System.import('./pages/homepage')} />
+            <AsyncRoute props={props} loadingPromise={import('./pages/homepage')} />
         )
     },
     {
         path: '/test',
         exact: true,
         component: (props) => (
-            <AsyncRoute props={props} loadingPromise={System.import('./pages/test')} />
+            <AsyncRoute props={props} loadingPromise={import('./pages/test')} />
         )
     },
     {
         path: '/test/sub',
         exact: false,
         component: (props) => (
-            <AsyncRoute props={props} loadingPromise={System.import('./pages/test-sub')} />
+            <AsyncRoute props={props} loadingPromise={import('./pages/test-sub')} />
         )
     }
 ]; // end of routes || DO NOT ALTER THIS LINE.
